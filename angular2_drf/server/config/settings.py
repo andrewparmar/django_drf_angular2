bcf27a51c4bb463291f7bfbea7c4eb9e286b37f1
Andrew Parmar <andrew.parmar@gmail.com>
2018-04-02 21:57:30 -0700

    Modified angular2drf to config folder. Renamed necessary files

--- angular2_drf/server/config/settings.py
+++ angular2_drf/server/config/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'angular2_drf.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'angular2_drf.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
