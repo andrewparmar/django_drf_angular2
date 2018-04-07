eb9484349a71105fe07a2a2724d44987523dc071
Andrew Parmar <andrew.parmar@gmail.com>
2018-04-07 12:37:23 -0700

    Created Rest api using DRF

--- angular2_drf/server/core/admin.py
+++ angular2_drf/server/core/admin.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Family, Transaction, Location
+
+admin.site.register(Product)
+admin.site.register(Family)
+admin.site.register(Transaction)
+admin.site.register(Location)
--- angular2_drf/server/core/views.py
+++ angular2_drf/server/core/views.py
@@ -3,4 +3,54 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Product , Location ,Family ,Transaction 
+from .serializers import *
+
+from rest_framework import status , generics , mixins
+
 # Create your views here.
+class product_list(generics.ListCreateAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ProductSerializer
+
+class product_detail(generics.RetrieveUpdateDestroyAPIView):
+	queryset = Product.objects.all()
+	serializer_class = ProductSerializer
+
+class family_list(generics.ListCreateAPIView):
+    queryset = Family.objects.all()
+    serializer_class = FamilySerializer
+
+# class family_list(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+#     queryset = Family.objects.all()
+#     serializer_class = FamilySerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+class family_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Family.objects.all()
+    serializer_class = FamilySerializer
+
+class location_list(generics.ListCreateAPIView):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class location_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Location.objects.all()
+    serializer_class =  LocationSerializer
+
+class transaction_list(generics.ListCreateAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
+
+class transaction_detail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class =  TransactionSerializer
